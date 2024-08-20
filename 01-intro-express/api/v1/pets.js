b8f31e9c90a48ffe0c449fc985d6d559ce3d089e
César Guerra <warderer@gmail.com>
2024-08-19 21:16:28 -0600

    ✨ params en express - con pets

--- 01-intro-express/api/v1/pets.js
+++ 01-intro-express/api/v1/pets.js
@@ -31,4 +31,19 @@ router.get('/api/v1/pets', (req, res) => {
     res.send(petList);
 });
 
+/* PARAMS */
+// Obtener parametros de una ruta dinámica (una mascota)
+// Ejemplo Params: '/api/v1/pets/1'
+// Para indicar que un valor es dinámico, se usa ':' antes del nombre del parámetro, es decir: '/api/v1/pets/:petId'
+router.get('/api/v1/pets/:petId', (req, res) => {
+    const petId = req.params.petId; // Obtener el valor del parámetro
+    const pet = petList.pets.find(pet => pet.id == petId); // Buscar la mascota con el id
+    if (pet) {
+        res.send(pet);
+    } else {
+        res.status(404).send({ message: 'Pet not found' });
+    }
+});
+
+
 module.exports = router; // Exportar el router para que pueda ser usado en otro archivo

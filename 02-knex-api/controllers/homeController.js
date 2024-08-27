5c2de24df4ad0133a430089a49e3143d305c2971
César Guerra <warderer@gmail.com>
2024-08-26 19:48:37 -0600

    ✨ Añadi Controlador CreateHome

---
+++ 02-knex-api/controllers/homeController.js
@@ -0,0 +1,29 @@
+/* CONTROLADORES */
+// Los controladores tienen la lógica de negocio de la aplicación.
+// Se encargan de validar datos, manejar errores y resolver promesas.
+
+// #1 Importar el modelo, en este caso Homes
+
+const ModelHomes = require('../models/Homes')
+
+// #2 Crear las funciones que van a ser llamadas en las rutas.
+// Create
+const createHome = (req, res) => {
+  ModelHomes.create(req.body).then((home) => {
+    res.status(201).json(home)
+  }).catch((error) => {
+    res.status(400).json(error.message)
+  })
+}
+
+// Read
+
+// Update
+
+// Delete
+
+// #3 Exportar las funciones para que sean accesibles desde las rutas (vista).
+
+module.exports = {
+  createHome
+}
